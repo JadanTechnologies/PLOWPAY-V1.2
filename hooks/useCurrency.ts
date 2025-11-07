1fb02e8b4e2b3af2de7f250b472b27b7d6ec5b55
Jabir Dangaskiya <jadanexpress.info@gmail.com>
2025-11-07 20:51:21 +0100

    feat: Improve currency formatting logic
    Refactors the `useCurrency` hook to utilize hardcoded currency data instead of relying on `systemSettings`. This simplifies the hook and ensures consistent currency formatting.

--- hooks/useCurrency.ts
+++ hooks/useCurrency.ts
@@ -1,13 +1,11 @@
-
-
 import { useAppContext } from './useAppContext';
-import { Currency } from '../types';
+import { allCurrencies } from '../utils/data';
 
 export const useCurrency = () => {
-  const { systemSettings, currentCurrency } = useAppContext();
+  const { currentCurrency } = useAppContext();
 
   const formatCurrency = (value: number) => {
-    const currency: Currency | undefined = systemSettings.currencies.find(c => c.code === currentCurrency);
+    const currency = allCurrencies.find(c => c.code === currentCurrency);
     const symbol = currency ? currency.symbol : '$';
 
     return `${symbol}${value.toFixed(2)}`;
